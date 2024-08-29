da91b7cb83d814f365886434d23c787920466c2a
Alberto Viezzi <aviezzi@outlook.com>
2024-08-29 17:49:56 +0200

    Add Libraries

---
+++ 02_Libraries/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var names = new List<string> { "Alice", "Bob", "Charlie" };
+        foreach (var name in names) Console.WriteLine(name);
+        
+        Console.ReadKey();
+    }
+}
